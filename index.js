bb7cb5fcf1d17df2766d5493d2701515bab9bab0
leenuri <leenuri1031@gmail.com>
2022-08-02 09:19:33 +0900

    JS_ver

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+import mainContent from "./module/main.js"
 
 const root = document.getElementById('root');
 const sections = root.children;
 
-// console.log(root);
+console.log(root);
 // console.log(sections.length);
 
 
 // sections[0].innerHTML = `<h1 class="title-text">El mundo's portfolio</h1>`;
+
+// console.log(mainContent);
+
+sections[1].innerHTML = mainContent ; 
 
