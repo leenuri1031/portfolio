ceed0d1a61f3d6b5e41e17f24a7815af3c95aae2
lee nuri <leenuri1031@gmail.com>
2023-12-29 14:59:17 +0900

    main_page_js_event_add_ing

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
-import mainContent from "./module/main.js"
+import { functionObj } from "./module/function_obj.js";
 
 const root = document.getElementById('root');
-// const sections = root.children;
+const container = document.getElementById('container');
 
-console.log(root);
-// console.log(sections.length);
+// console.log(root);
+// console.dir(root);
+// console.log(container.children[0]);
+
+const menuBox = container.children[1];
+// console.log(menuBox);
+// console.dir(menuBox);
+
+let mainMenu = document.getElementById('list');
+// console.log(mainMenu);
+// console.dir(mainMenu.children);
+
+const menuList = [mainMenu.children[0], mainMenu.children[1], mainMenu.children[2], mainMenu.children[3], mainMenu.children[4]];
+
+// console.log(Array.isArray(menuList));
+console.dir(menuList[0]);
+
+console.log(menuList[0].style.width);
+
+// menuList[0].addEventListener('mouseenter', (e)=>{
+//   console.log('mouse-enter-event');
+//   functionObj.size(menuList[0], '250px', '250px');
+// });
+
+// menuList[0].addEventListener('mouseleave', (e)=>{
+//   console.log('mouse-leave-event');
+//   functionObj.size(menuList[0], '150px', '150px');
+// });
+
+// functionObj.size(menuList[0], '250px', '250px');
+
+// for(let i = 0; i < 5; i++){
+//   console.log(menuList);
+// }
 
 
-// sections[0].innerHTML = `<h1 class="title-text">El mundo's portfolio</h1>`;
-
-// console.log(mainContent);
-
-// sections[1].innerHTML = mainContent ; 
 
